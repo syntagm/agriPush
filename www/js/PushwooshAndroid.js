1cb4a6f4558cfa5b2b58d1f18f16c885b1f2b865
syntagm <syntagm@icon.co.za>
2015-08-25 14:29:41 +0200

    upload 0825_01
    new project id

--- www/js/PushwooshAndroid.js
+++ www/js/PushwooshAndroid.js
@@ -42,7 +42,7 @@ function registerPushwooshAndroid() {
 	);
 
 	//initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
-	pushNotification.onDeviceReady({ projectid: "505908946299", appid : "6D00F-54EB3" });
+	pushNotification.onDeviceReady({ projectid: "514629060280", appid : "6D00F-54EB3" });
 
 	//register for push notifications
 	pushNotification.registerDevice(
